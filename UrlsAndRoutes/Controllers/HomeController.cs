d7b9f7fb6f47208ffa996ff665c96d024bfce2c3
Димитар Илиев <iliev.dimitar@students.finki.ukim.mk>
2019-03-05 22:26:36 +0100

    Routing example implementation.

---
+++ UrlsAndRoutes/Controllers/HomeController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using UrlsAndRoutes.Models;
+
+namespace UrlsAndRoutes.Controllers
+{
+    public class HomeController: Controller
+    {
+        public ViewResult Index() => View("Result", new Result
+        {
+            Controller = nameof(HomeController),
+            Action = nameof(Index)
+        });
+
+        public ViewResult CustomVariable(string id)
+        {
+            Result result = new Result
+            {
+                Controller = nameof(HomeController),
+                Action = nameof(CustomVariable)
+            };
+
+            result.Data["id"] = id ?? "<no value>";
+            return View("Result", result);
+        }
+    }
+}
--- UrlsAndRoutes/Startup.cs
+++ UrlsAndRoutes/Startup.cs
@@ -15,19 +15,32 @@ namespace UrlsAndRoutes
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMvc();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
+            app.UseStatusCodePages();
+            app.UseDeveloperExceptionPage();
+            app.UseStaticFiles();
+            app.UseMvc(routes => {
+                routes.MapRoute(name: "default", template: "{controller=Home}/{action=Index}");//, defaults: new { action = "Index" });
+                // example route for prefixed Urls
+                // routes.MapRoute("Public/{controller=Home}/{action=Index}");
+                // url patter that has both static and variable elements
+                // routes.MapRoute("", "X{controller}/{action});
 
-            app.Run(async (context) =>
-            {
-                await context.Response.WriteAsync("Hello World!");
+                // route that has controller replaced with another
+                // routes.MapRoute(name: "ShopSchema", template: "Shop/{action}", defaults: new { controller = "Home" });
+                // controller and action replaced
+                // routes.MapRoute(name: "ShopSchema2", template: "Shop/OldAction", defaults: new { controller = "Home", action = "Index" });
+
+                // defining custom segment variables
+                // routes.MapRoute(name: "MyRoute", template: "{controller=Home}/{action=Index}/{id=DefaultId}");
+
+                //specifying optional url segment
+                // routes.MapRoute(name: "MyRoute", template: "{controller=Home}/{action=Index}/{id?}");
             });
         }
     }
